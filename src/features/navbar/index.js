fbab234c11bf2c82a247e94aab13e9986d4437ae
Easykatka <secondjump@bk.ru>
2020-12-31 18:50:30 +0300

    fix

--- src/features/navbar/index.js
+++ src/features/navbar/index.js
@@ -2,11 +2,7 @@ import { NavLink } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import { useState } from "react";
+
 
 const useStyles = makeStyles((theme) => ({
   navbar__container: {},
@@ -18,39 +14,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const Navbar = () => {
+
   const classes = useStyles();
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
   return (
-    // <Tabs
-    //   orientation="vertical"
-    //   value={value}
-    //   onChange={handleChange}
-    //   className={classes.tabs}
-    // >
-    //   <NavLink to="/profile">
-    //     <Tab label="My Profile" />
-    //   </NavLink>
-    //   <NavLink to="/messages">
-    //     <Tab label="Messages" />{" "}
-    //   </NavLink>
-	//   <NavLink to="/users">
-    //     <Tab label="Users" />{" "}
-    //   </NavLink>
-	//   <NavLink to="/chat">
-    //     <Tab label="Chat" />{" "}
-    //   </NavLink>
-
-    // </Tabs>
-
       <Grid
         direction="column"
         container
         className={classes.navbar__container}
     	spacing={2}
-
       >
         <Grid  item xs>
           <NavLink  to="/profile" activeClassName="active">
