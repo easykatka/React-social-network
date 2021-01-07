e145276d5dd6787d5c4e42cc52a07d24b7e4a803
Easykatka <secondjump@bk.ru>
2021-01-08 00:22:08 +0300

    added websocket

--- src/features/chat/index.js
+++ src/features/chat/index.js
@@ -1,7 +1,73 @@
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 export const Chat = () => {
-	return (
-		<div>chat</div>
-	)
-}
+	
+  return (<>
+	<ChatMessages />
+	<AddMessageForm />
+	</>
+  )
+};
+const ChatMessages = () => {
+  
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+	const ws = new WebSocket(
+		"wss://social-network.samuraijs.com/handlers/ChatHandler.ashx"
+	  );
+    ws.addEventListener("message", (e) => {
+      const newMessages = JSON.parse(e.data);
+      setMessages((prev) => [...prev, ...newMessages]);
+    });
+  }, []);
+
+  console.log(messages);
+
+  return (
+    <div
+      style={{
+        height: "500px",
+        overflowY: "auto",
+      }}
+    >
+      {messages.map((i, index) => (
+        <Message key={index} message={i} />
+      ))}
+    </div>
+  );
+};
+
+const Message = ({ message }) => {
+  return (
+    <div>
+      <img src={message.photo} style={{ width: "30px" }} />{" "}
+      <b>{message.userName}</b>
+      <br />
+      {message.message}
+      <hr />
+    </div>
+  );
+};
+const AddMessageForm = () => {
+	const ws = new WebSocket(
+		"wss://social-network.samuraijs.com/handlers/ChatHandler.ashx"
+	  );
+	const [message, setMessage] = useState("");
+  const SendMessage = () => {if (message) 
+	  {ws.send(message)} setMessage('')
+  }
+  return (
+    <>
+      <div>
+        <textarea name="message" onChange={(e) =>setMessage(e.currentTarget.value) } value={message} id="" cols="30" rows="1">
+          d
+        </textarea>{" "}
+      </div>
+      <div>
+        <button onClick={SendMessage}>Send</button>
+      </div>
+    </>
+  );
+};
